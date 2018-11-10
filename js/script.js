791658ca5feeb32af7e04cf5fcfa789e7adfc25c
Adrian Hawkes <hawkes.adrian@gmail.com>
2018-11-10 13:49:15 +0000

    Setting up initial file structure to use chart.js library

---
+++ js/script.js
@@ -0,0 +1,19 @@
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'bar',
+
+    // The data for our dataset
+    data: {
+        labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+        datasets: [{
+            label: "My First dataset",
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [10, 10, 5, 2, 20, 30, 45, 2, 25, 26, 2, 29],
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
